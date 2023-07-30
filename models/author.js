f1102b9f27be882d90dd77655b86c2326869ca26
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-30 15:40:08 +0200

    rendering detail pages, part 5

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -25,6 +26,16 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("birth_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : 'no data';
+});
+
+AuthorSchema.virtual("death_formatted").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : 'no data';
 });
 
 // Export model
